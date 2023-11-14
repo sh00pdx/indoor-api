5e71a373dfeda1f801b776c0d31300e14787eaba
Marcos Pulgar <marcos.pulgar@autofact.cl>
2023-11-14 10:16:34 -0300

    try handle conextion

--- app/middleware/error_handler/wrap.py
+++ app/middleware/error_handler/wrap.py
@@ -1,17 +1,20 @@
 from fastapi.responses import JSONResponse
 from functools import wraps
 import inspect
-
+from app.models.connection_builder import database
 def error_handler_decorator(callback=None):
     def decorator(func):
         @wraps(func)
         async def wrapper(*args, **kwargs):
             try:
+                database.connect(reuse_if_open=True)
                 # Llamar a la función original
                 result = await func(*args, **kwargs)
                 # Todo salió bien, devolver una respuesta JSON con código 200
+                database.close()
                 return JSONResponse(content={"data": result}, status_code=200)
             except Exception as e:
+                database.close()
                 # Algo salió mal, ejecutar el callback y levantar la excepción
                 if callback:
                     # Comprobar si el callback es síncrono o asíncrono
